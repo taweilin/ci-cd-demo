fb341128dce3f5fabf3127235bac36f40d4c7b04
taweilin <taweilin@gmail.com>
2025-04-24 15:30:46 +0800

    修正測試中的異步操作問題

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// 創建 app.js 檔案
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,8 +6,11 @@ app.get("/", (req, res) => {
   res.send("Hello CI/CD World!");
 });
 
-app.listen(port, () => {
-  console.log(`應用程式執行於 http://localhost:${port}`);
-});
+// 只有在直接執行此檔案時才啟動伺服器
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`應用程式執行於 http://localhost:${port}`);
+  });
+}
 
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,18 @@
-// 創建測試檔案
 const request = require("supertest");
 const app = require("../app");
+
+// 保存伺服器實例以便後續關閉
+let server;
+
+// 測試前啟動伺服器
+beforeAll(() => {
+  server = app.listen(4000);
+});
+
+// 測試後關閉伺服器
+afterAll((done) => {
+  server.close(done);
+});
 
 describe("GET /", () => {
   it("應該回傳 Hello CI/CD World!", async () => {
